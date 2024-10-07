35146225d2e84b757b88d182fccaaa0403a20c12
Brant Robertson <brantr@users.noreply.github.com>
2024-10-07 12:02:29 -0700

    Changed names

--- run_parse_megasecond.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm nircam_*.txt
-rm nirspec_*.txt
-rm ns_layer_group.txt
-rm nc_layer_group.txt
-python3 parse_apt_pointing.py -i megasec.pointing --nirspec nirspec.txt --nircam nircam.txt --fits f200w_SCI.fits
